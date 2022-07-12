9cf8bd3e6a4ed0e37d89532c6927d72e3c3dc67c
unstoppableTANMAY <iamunstoppableguy@gmail.com>
2022-07-12 19:21:56 +0530

    Pushing The Note Taking App .

---
+++ stacks/index.js
@@ -0,0 +1,13 @@
+import { StorageStack } from "./StorageStack";
+import { ApiStack } from "./ApiStack";
+
+export default function main(app) {
+  app.setDefaultFunctionProps({
+    runtime: "nodejs16.x",
+    srcPath: "services",
+    bundle: {
+      format: "esm",
+    },
+  });
+  app.stack(StorageStack).stack(ApiStack);
+}
