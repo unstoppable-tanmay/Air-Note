5e32dbd74870807f971f4c8606a77bce3a61c97d
unstoppableTANMAY <iamunstoppableguy@gmail.com>
2022-07-13 14:23:32 +0530

    Securing the API

--- services/functions/list.js
+++ services/functions/list.js
@@ -1,7 +1,7 @@
 import handler from "../util/handler";
 import dynamoDb from "../util/dynamodb";
 
-export const main = handler(async () => {
+export const main = handler(async (event) => {
   const params = {
     TableName: process.env.TABLE_NAME,
     // 'KeyConditionExpression' defines the condition for the query
@@ -11,7 +11,8 @@ export const main = handler(async () => {
     // 'ExpressionAttributeValues' defines the value in the condition
     // - ':userId': defines 'userId' to be the id of the author
     ExpressionAttributeValues: {
-      ":userId": "123",
+      // ":userId": "123",
+      ":userId": event.requestContext.authorizer.iam.cognitoIdentity.identityId,
     },
   };
 
@@ -20,3 +21,8 @@ export const main = handler(async () => {
   // Return the matching list of items in response body
   return result.Items;
 });
+
+
+// npx aws-api-gateway-cli-test --username=tanmaypanda752@gmail.com --password=Tanmay@123 --user-pool-id=us-east-1_3L8ZTHyyd --app-client-id=4gafru7hej1749nn278c0ukabb --cognito-region=us-east-1 --identity-pool-id=us-east-1:9509fd27-c072-402e-8b69-4c7e23491cf7 --invoke-url="https://t48ylb3j8f.execute-api.us-east-1.amazonaws.com"--api-gateway-region=us-east-1 --path-template=/notes --method=POST --body="{"content":"hello world","attachment":"hello.jpg"}"
+
+// npx aws-api-gateway-cli-test --username tanmaypanda752@gmail.com --password Tanmay@123 --user-pool-id us-east-1_3L8ZTHyyd --app-client-id 4gafru7hej1749nn278c0ukabb --cognito-region us-east-1 --identity-pool-id us-east-1:9509fd27-c072-402e-8b69-4c7e23491cf7 --invoke-url https://t48ylb3j8f.execute-api.us-east-1.amazonaws.com --api-gateway-region us-east-1 --path-template /notes --method POST --body "{\"content\":\"hello world\",\"attachment\":\"hello.jpg\"}"
