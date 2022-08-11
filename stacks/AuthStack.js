b470fbd54f5939772d17d7ab41214f8b5cf5cff8
unstoppableTANMAY <iamunstoppableguy@gmail.com>
2022-08-11 22:39:46 +0530

    changing callback uri

--- stacks/AuthStack.js
+++ stacks/AuthStack.js
@@ -26,8 +26,8 @@ export function AuthStack({ stack, app }) {
           cognito.UserPoolClientIdentityProvider.GOOGLE,
         ],
         oAuth: {
-          callbackUrls: ["https://air-note.netlify.app/"],
-          logoutUrls: ["https://air-note.netlify.app/"],
+          callbackUrls: ["https://air-note.netlify.app"],
+          logoutUrls: ["https://air-note.netlify.app"],
         },
       },
     },
