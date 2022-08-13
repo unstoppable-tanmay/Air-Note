121706f5fd85e5d868d2eb4ed1e469943cbaa9fe
unstoppableTANMAY <iamunstoppableguy@gmail.com>
2022-08-13 16:48:31 +0530

    updating the ses 2

--- stacks/Sendemail.js
+++ stacks/Sendemail.js
@@ -2,6 +2,6 @@ import { Function } from "@serverless-stack/resources";
 
 export function Sendemail({ stack, app }) {
   new Function(stack, "email", {
-    handler: "../services/functions/signupemail.handler",
+    handler: "../services/functions/sendemail.handler",
   });
 }
